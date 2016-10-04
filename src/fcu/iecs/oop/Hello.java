25bfaf614466c084be74f9f049f1bd031f1371a3
fcu-d0426554 <zandy763628039@gmail.com>
2016-10-05 02:09:02 +0800

    Homework 1
    Let her go! Let her go! Can't hold her back anymore

--- src/fcu/iecs/oop/Hello.java
+++ src/fcu/iecs/oop/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello FCU");
+		String lyrics ="Let her go! Let her go! Can't hold her back anymore";
 	}
 
 }
